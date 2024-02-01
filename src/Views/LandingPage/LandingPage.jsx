c33a52af7d72308b2d78a30800b2c2b3a3236e4b
Noah Greensweig <ngreensweig@gmail.com>
2024-01-31 18:28:43 -0600

    Add background photo and ButtonComponent to LandingPage view.

--- src/Views/LandingPage/LandingPage.jsx
+++ src/Views/LandingPage/LandingPage.jsx
@@ -1,6 +1,38 @@
+import ButtonComponent from "../../Components/Button/Button";
+
 function LandingPage() {
     return (
-        <p class='text-red-400'>Home</p>
+        <div
+            className='instant-quote-hero'
+            style={{
+                position: 'relative',
+                height: '100vh',
+                backgroundSize: 'cover'
+            }}
+        >
+            <div
+                style={{
+                    position: 'absolute',
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    bottom: 0,
+                    backgroundImage: "url('images/cedar/craftsman/craftsman-1.jpeg')",
+                    backgroundSize: 'cover',
+                    opacity: .3,
+                    zIndex: -1,
+                }}
+            />
+            <div
+                className='content'
+                style={{
+                    position: 'relative', zIndex: 1, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center',
+                }}
+            >
+                <ButtonComponent type="Quote" />
+            </div>
+
+        </div >
     )
 }
 
