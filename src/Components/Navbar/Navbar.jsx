c52ac956ab69a2ce8b3c3b63177c2ddad0d545f7
Noah Greensweig <ngreensweig@gmail.com>
2024-02-01 12:06:54 -0600

    Added links to navbar.

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,32 +1,50 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import {
+    HashRouter as Router,
+    Route,
+} from 'react-router-dom'; // import for routing
 
 const Navbar = () => {
     return (
-        <nav className='bg-nates-dark-blue text-white flex flex-row justify-between'>
-            <div>
-                <img src='../images/logos/nates-logo.jpg' className='h-28 ml-2'></img>
-            </div>
-            <ul className='h-28 flex flex-row justify-between items-center gap-x-48'>
-                <li className=''>
-                    Home
-                </li>
-                <li className=''>
-                    About
-                </li>
-                <li lassName=''>
-                    Fence Types
-                </li>
-                <li className=''>
-                    Instant Quote
-                </li>
-                <li className='mr-10'>
-                    FAQ
-                </li>
+        <Router>
+            <nav className='bg-nates-dark-blue text-white flex flex-row justify-between'>
+                <div>
+                    <Link to='/'>
+                        <img src='../images/logos/nates-logo.jpg' className='h-28 ml-2'></img>
+                    </Link>
+                </div>
+                <ul className='h-28 flex flex-row justify-between items-center gap-x-48'>
+                    <li className=''>
+                        <Link to='/'>
+                            Home
+                        </Link>
+                    </li>
+                    <li className=''>
+                        <Link to='/about'>
+                            About
+                        </Link>
+                    </li>
+                    <li className=''>
+                        <Link to='/fence-types'>
+                            Fence Types
+                        </Link>
+                    </li>
+                    <li className=''>
+                        <Link to='/quote'>
+                            Instant Quote
+                        </Link>
+                    </li>
+                    <li className='mr-10'>
+                        <Link to='/faq'>
+                            FAQ
+                        </Link>
+                    </li>
 
 
-            </ul>
-        </nav>
+                </ul>
+            </nav>
+        </Router>
     );
 };
 
