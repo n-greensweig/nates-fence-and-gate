dfd670eb2f5a6f3c4c31d1c69cd38908daef8efd
Noah Greensweig <ngreensweig@gmail.com>
2024-02-09 10:23:39 -0600

    Added links to the items in the lower left hand side of the Footer component.

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -6,6 +6,16 @@ const Footer = () => {
         <footer className='bg-nates-dark-blue text-white flex flex-row justify-evenly border-t-8 border-nates-light-blue mt-14'>
             <div className='h-64 w-1/3'>
                 <h3 className='text-5xl'>Contact</h3>
+                <div>
+                    <h2>Contact us:</h2>
+                    <p>
+                        <Link to='/' onClick={() => window.scrollTo(0, 0)}>Home</Link> |{' '}
+                        <Link to='/fence-types' onClick={() => window.scrollTo(0, 0)}>Fence Types</Link> |{' '}
+                        <a href='https://www.google.com/search?q=nates+fence+and+gate&oq=nates&gs_lcrp=EgZjaHJvbWUqCAgAEEUYJxg7MggIABBFGCcYOzIUCAEQRRgUGDkYgwEYhwIYsQMYgAQyDwgCEAAYFBiHAhiLAxiABDINCAMQLhivARjHARiABDIJCAQQABgKGIAEMgYIBRBFGDwyBggGEEUYPDIGCAcQRRhB0gEHODM4ajFqN6gCALACAA&sourceid=chrome&ie=UTF-8#lrd=0x52b33be89d42efcd:0xfa2f5923965e7add,1,,,,' target='_blank'>Google Reviews</a> |{' '}
+                        <Link to='/quote' onClick={() => window.scrollTo(0, 0)}>Instant Quote</Link> |{' '}
+                        <Link to='/about' onClick={() => window.scrollTo(0, 0)}>About</Link>
+                    </p>
+                </div>
             </div>
 
             <div className='h-64 w-1/3'>
@@ -24,7 +34,7 @@ const Footer = () => {
                         <p>Circle Pines</p>
                         <p>Columbia Heights</p>
                     </div>
-                    <div  style={{ textAlign: 'center' }}>
+                    <div style={{ textAlign: 'center' }}>
                         <p>Coon Rapids</p>
                         <p>Crystal</p>
                         <p>Falcon Heights</p>
