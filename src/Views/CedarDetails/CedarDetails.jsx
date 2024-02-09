69b8f2605e86325fc1bb9aa45b0c1bb11ca27c19
Noah Greensweig <ngreensweig@gmail.com>
2024-02-09 16:44:11 -0600

    Restyled FenceTypes view to fit 4 cards per row on the DOM and CedarDetails to fit 3 cards per row on the DOM.

--- src/Views/CedarDetails/CedarDetails.jsx
+++ src/Views/CedarDetails/CedarDetails.jsx
@@ -14,10 +14,10 @@ function CedarDetails() {
     const isSmScreen = useMediaQuery(theme.breakpoints.down('sm'));
 
     return (
-        <div className="container" style={{ display: 'flex', justifyContent: 'center', padding: '20px', margin: '0 auto' }}>
+        <div className="container" style={{ display: 'flex', justifyContent: 'center', padding: '20px', margin: '0 auto', }}>
             <Grid container spacing={2} style={{ maxWidth: '1400px' }}>
                 {cedarTypes.map((cedarType, index) => (
-                    <Grid item xs={12} sm={6} md={4} lg={3} key={index}>
+                    <Grid item xs={12} sm={6} md={4} lg={4} key={index}>
                         <TypeCard fenceType={cedarType} />
                     </Grid>
                 ))}
