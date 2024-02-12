b31d311470ef877cd53190fa5498f58d9b3d3e67
mh <maxhalverson@live.com>
2024-02-12 17:08:57 -0600

    updated tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,8 +16,17 @@ module.exports = {
         40: '.40',
       },
       height: {
-        '50vh': 'height: 50vh'
+        '50vh': 'height: 50vh',
       },
+      keyframes: {
+        'bounce-fast': {
+          '0%, 100%': { transform: 'translatey(-0.25rem)' },
+          '50%': { transform: 'translatey(1rem)' }
+        }
+      },
+      animation: {
+        'bounce-fast': 'bounce-fast 1s ease-in infinite',
+      }
     },
   },
   plugins: [],
