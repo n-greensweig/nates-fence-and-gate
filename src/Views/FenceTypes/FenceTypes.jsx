ef91131e1a4b4a4ea4f78cf3f158fd66c24a8d20
Max Halverson <maxhalverson@Maxs-MacBook-Pro.local>
2024-02-13 18:02:27 -0600

    set up screen size breakpoints. Changed layout of fence types page hero text to be responsive

--- src/Views/FenceTypes/FenceTypes.jsx
+++ src/Views/FenceTypes/FenceTypes.jsx
@@ -7,58 +7,74 @@ import { fenceTypes } from "../../Constants/Constants"; // global fenceTypes var
 // Responsive imports
 import { useMediaQuery, useTheme } from "@mui/material";
 
-
 function FenceTypes() {
+  useEffect(() => {
+    document.title = "Fence Types";
+  }, []);
 
-    useEffect(() => {
-        document.title = "Fence Types";
-    }, []);
+  // Check the screen size for responsive design
+  const theme = useTheme();
+  const isXsScreen = useMediaQuery(theme.breakpoints.down("xs"));
+  const isSmScreen = useMediaQuery(theme.breakpoints.down("sm"));
 
-    // Check the screen size for responsive design
-    const theme = useTheme();
-    const isXsScreen = useMediaQuery(theme.breakpoints.down('xs'));
-    const isSmScreen = useMediaQuery(theme.breakpoints.down('sm'));
-
-    return (
-        <div className='h-screen w-screen'>
-            <div className='bg-hero-fence-types h-1/2 flex relative'>
-                <h1 className='text-7xl text-white absolute absolute start-1/2 -translate-x-1/2 bottom-2/3 translate-y-1/3'>Fence Types</h1>
-                <p className='text-white text-3xl font-thin absolute start-1/2 -translate-x-1/2 bottom-1/2 translate-y-1/2'>
-                    Choosing the perfect fence is as unique as your needs and interests.
-                    Explore our wide range of fence types, tailored to match your individual
-                    requirements and preferences.
-                </p>
-            </div>
-            <div className="content" style={{ marginTop: isXsScreen || isSmScreen ? '5%' : '3%', fontSize: '2rem' }}>
-                <Grid container spacing={2} minHeight={'5vh'} xs={12} sm={6} md={4} lg={3}
-                    style={{
-                        marginTop: '0px',
-                        display: 'flex',
-                        flexDirection: 'row',
-                        flexWrap: 'wrap',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        maxWidth: '1400px',
-                        margin: '0 auto',
-                        padding: '20px 10px',
-                    }}
-                >
-                    {fenceTypes.map((fenceType, index) => {
-                        return (
-                            <Grid item xs={12} sm={6} md={4} lg={3} key={index}
-                                style={{
-                                    height: '500px', // Make wider than it is tall on small screens?
-                                }}
-                            >
-                                <TypeCard key={index} fenceType={fenceType} />
-                            </Grid>
-                        )
-                    })}
-                </Grid>
-
-            </div>
-        </div>
-    )
+  return (
+    <div className="h-screen w-screen">
+      <div className="bg-hero-fence-types h-1/2 flex flex-col justify-center">
+        <h1 className="text-7xl text-white mb-5">Fence Types</h1>
+        <p className="text-white text-3xl font-thin">
+          Choosing the perfect fence is as unique as your needs and interests.
+          Explore our wide range of fence types, tailored to match your
+          individual requirements and preferences.
+        </p>
+      </div>
+      <div
+        className="content"
+        style={{
+          marginTop: isXsScreen || isSmScreen ? "5%" : "3%",
+          fontSize: "2rem",
+        }}
+      >
+        <Grid
+          container
+          spacing={2}
+          minHeight={"5vh"}
+          xs={12}
+          sm={6}
+          md={4}
+          lg={3}
+          style={{
+            marginTop: "0px",
+            display: "flex",
+            flexDirection: "row",
+            flexWrap: "wrap",
+            justifyContent: "center",
+            alignItems: "center",
+            maxWidth: "1400px",
+            margin: "0 auto",
+            padding: "20px 10px",
+          }}
+        >
+          {fenceTypes.map((fenceType, index) => {
+            return (
+              <Grid
+                item
+                xs={12}
+                sm={6}
+                md={4}
+                lg={3}
+                key={index}
+                style={{
+                  height: "500px", // Make wider than it is tall on small screens?
+                }}
+              >
+                <TypeCard key={index} fenceType={fenceType} />
+              </Grid>
+            );
+          })}
+        </Grid>
+      </div>
+    </div>
+  );
 }
 
 export default FenceTypes;
