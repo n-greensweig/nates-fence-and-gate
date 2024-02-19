a5fb9284ea778076496b58ab2b47ff6b655d86ea
Noah Greensweig <ngreensweig@gmail.com>
2024-02-19 14:25:09 -0600

    Added custom menu icon.

---
+++ src/Components/MobileNavbar/MobileNavbar.jsx
@@ -0,0 +1,125 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import { Button } from '@mui/material';
+import { slide as Menu } from 'react-burger-menu'; // Import the slide animation
+import MenuIcon from '@mui/icons-material/Menu';
+import MenuOpenIcon from '@mui/icons-material/MenuOpen';
+
+// Responsive imports
+import { useMediaQuery } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+
+function MobileNavbar() {
+
+    const [showDropdown, setShowDropdown] = useState(false);
+    const [menuOpen, setMenuOpen] = useState(false);
+    const toggleDropdown = () => setShowDropdown(!showDropdown);
+    const toggleMenu = () => setMenuOpen(!menuOpen);
+
+    const handleStateChange = state => setMenuOpen(state.isOpen);
+    const closeMenu = () => setMenuOpen(false);
+
+    // Define styles for the burger menu
+    var styles = {
+        bmBurgerButton: {
+            position: 'fixed',
+            width: '36px',
+            height: '30px',
+            left: '36px',
+            top: '36px',
+        },
+        bmCrossButton: {
+            height: '24px',
+            width: '24px'
+        },
+        bmCross: {
+            background: '#bdc3c7'
+        },
+        bmMenu: {
+            background: '#373a47',
+            padding: '2.5em 1.5em 0',
+            fontSize: '1.15em'
+        },
+        bmItemList: {
+            color: '#b8b7ad',
+            padding: '0.8em',
+            display: 'flex',
+            flexDirection: 'column', // Align items vertically
+            alignItems: 'center', // Center align items horizontally
+        },
+        bmItem: {
+            display: 'inline-block'
+        },
+        bmOverlay: {
+            background: 'rgba(0, 0, 0, 0.3)'
+        }
+    };
+
+
+    return (
+        <div>
+            {/* Trigger Button (use fixed positioning) */}
+            <Button
+                style={{ ...styles.bmBurgerButton, position: 'fixed', zIndex: 999 }}
+                onClick={toggleMenu}
+                startIcon={menuOpen ?
+                    <MenuOpenIcon style={{ color: 'white', fontSize: '4rem' }} /> :
+                    <MenuIcon style={{ color: 'white', fontSize: '4rem' }} />
+                }
+            >
+                <i className="fas fa-bars" style={{ color: 'white', visibility: 'visible' }}></i>
+            </Button>
+
+            <Menu
+                isOpen={menuOpen}
+                onStateChange={handleStateChange}
+                styles={styles}
+                left
+                className='hide'
+                >
+                <Link to="/" onClick={closeMenu}>Home</Link>
+                <Link to="/fence-types" onClick={toggleDropdown}>Fence Types</Link>
+                {showDropdown && (
+                    <ul className="menu-dropdown bg-white text-black mt-1 p-2 rounded-lg shadow-lg">
+                        <Link to="/fence-details/cedar" onClick={closeMenu}>
+                            <li className='dropdown-item'>Cedar</li>
+                        </Link>
+                        <Link to="/fence-details/vinyl" onClick={closeMenu}>
+                            <li className='dropdown-item'>Vinyl</li>
+                        </Link>
+                        <Link to="/fence-details/ornamental" onClick={closeMenu}>
+                            <li className='dropdown-item'>Ornamental</li>
+                        </Link>
+                        <Link to="/fence-details/chain-link" onClick={closeMenu}>
+                            <li className='dropdown-item'>Chain Link</li>
+                        </Link>
+                    </ul>
+                )}
+                <Link to="/about" onClick={closeMenu}>About</Link>
+                <Link to="/quote" onClick={closeMenu}>
+                    <Button style={{
+                        backgroundColor: 'white',
+                        color: '#0283AB',
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        border: '2px solid #0283AB',
+                        fontWeight: 'bold',
+                        borderRadius: '5px',
+                        padding: '0 1rem',
+                        textTransform: 'none',
+                        transition: 'opacity 0.3s',
+                    }}
+                        onMouseEnter={e => e.target.style.opacity = 0.8}
+                        onMouseLeave={e => e.target.style.opacity = 1}
+                    >
+                        Instant Quote
+                    </Button>
+                </Link>
+            </Menu>
+        </div>
+    );
+}
+
+export default MobileNavbar;
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,173 +5,87 @@ import './Navbar.css';
 import { Button } from '@mui/material';
 import { slide as Menu } from 'react-burger-menu'; // Import the slide animation
 
+import MobileNavbar from '../MobileNavbar/MobileNavbar';
+
 // Responsive imports
 import { useMediaQuery } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
 
 const Navbar = () => {
     const [showDropdown, setShowDropdown] = useState(false);
-    const [menuOpen, setMenuOpen] = useState(false);
 
     const toggleDropdown = () => setShowDropdown(!showDropdown);
-
-    const handleStateChange = (state) => {
-        setMenuOpen(state.isOpen);
-    };
-
-    const closeMenu = () => {
-        setMenuOpen(false);
-    };
-
-    // Define styles for the burger menu
-    var styles = {
-        bmBurgerButton: {
-            position: 'absolute',
-            width: '36px',
-            height: '30px',
-            left: '36px',
-            top: '36px',
-        },
-        bmBurgerBars: {
-            background: '#fff'
-        },
-        bmCrossButton: {
-            height: '24px',
-            width: '24px'
-        },
-        bmCross: {
-            background: '#bdc3c7'
-        },
-        bmMenu: {
-            background: '#373a47',
-            padding: '2.5em 1.5em 0',
-            fontSize: '1.15em'
-        },
-        bmItemList: {
-            color: '#b8b7ad',
-            padding: '0.8em',
-            display: 'flex',
-            flexDirection: 'column', // Align items vertically
-            alignItems: 'center', // Center align items horizontally
-        },
-        bmItem: {
-            display: 'inline-block'
-        },
-        bmOverlay: {
-            background: 'rgba(0, 0, 0, 0.3)'
-        }
-    };
 
     // Check the screen size for responsive design
     const theme = useTheme();
     const isXsScreen = useMediaQuery(theme.breakpoints.down('xs'));
     const isSmScreen = useMediaQuery(theme.breakpoints.down('sm'));
+    const isMdScreen = useMediaQuery(theme.breakpoints.down('md'));
+    const isLgScreen = useMediaQuery(theme.breakpoints.down('lg'));
 
     return (
         <Router>
             <nav className='w-full bg-nates-dark-blue text-white flex flex-row justify-between items-center'>
-                {isXsScreen || isSmScreen ? null : <div>
+                {isXsScreen || isSmScreen || isMdScreen || isLgScreen ? null : <div>
                     <Link to='/'>
                         <img src='../images/logos/nates-logo.jpg' className='h-28 ml-2 '></img>
                     </Link>
                 </div>}
-                <Menu
-                    isOpen={menuOpen}
-                    onStateChange={handleStateChange}
-                    styles={styles}
-                    left>
-                    <Link to="/" onClick={closeMenu}>Home</Link>
-                    <Link to="/fence-types" onClick={toggleDropdown}>Fence Types</Link>
-                    {showDropdown && (
-                        <ul className="menu-dropdown bg-white text-black mt-1 p-2 rounded-lg shadow-lg">
-                            <Link to="/fence-details/cedar" onClick={closeMenu}>
-                                <li className='dropdown-item'>Cedar</li>
+
+                {isXsScreen || isSmScreen || isMdScreen || isLgScreen ? <MobileNavbar /> :
+
+                    <ul className='h-28 flex flex-row justify-between items-center gap-x-48 mr-4'>
+                        <li className='nav-link text-xl'>
+                            <Link to='/'>Home</Link>
+                        </li>
+                        <li className='relative nav-link text-thin text-xl' onMouseEnter={toggleDropdown} onMouseLeave={toggleDropdown}>
+                            <Link to='/fence-types'>Fence Types</Link>
+                            {showDropdown && (
+                                <ul className="absolute bg-white mt-1 p-2 rounded-lg shadow-lg blue-border forefront">
+                                    <Link to="/fence-details/cedar">
+                                        <li className='dropdown-item'>Cedar</li>
+                                    </Link>
+                                    <Link to="/fence-details/vinyl">
+                                        <li className='dropdown-item'>Vinyl</li>
+                                    </Link>
+                                    <Link to="/fence-details/ornamental">
+                                        <li className='dropdown-item'>Ornamental</li>
+                                    </Link>
+                                    <Link to="/fence-details/chain-link">
+                                        <li className='dropdown-item no-bottom'>Chain Link</li>
+                                    </Link>
+                                </ul>
+                            )}
+                        </li>
+                        <li className='nav-link text-xl'>
+                            <Link to='/about'>About</Link>
+                        </li>
+                        <li style={{ height: '50%' }}>
+                            <Link to='/quote'>
+                                <Button style={{
+                                    backgroundColor: 'white',
+                                    color: '#0283AB',
+                                    height: '100%',
+                                    display: 'flex',
+                                    alignItems: 'center',
+                                    justifyContent: 'center',
+                                    border: '2px solid #0283AB',
+                                    fontWeight: 'bold',
+                                    borderRadius: '5px',
+                                    padding: '0 1rem',
+                                    textTransform: 'none',
+                                    transition: 'opacity 0.3s',
+                                }}
+                                    onMouseEnter={e => e.target.style.opacity = 0.8}
+                                    onMouseLeave={e => e.target.style.opacity = 1}
+                                >
+                                    Instant Quote
+                                </Button>
                             </Link>
-                            <Link to="/fence-details/vinyl" onClick={closeMenu}>
-                                <li className='dropdown-item'>Vinyl</li>
-                            </Link>
-                            <Link to="/fence-details/ornamental" onClick={closeMenu}>
-                                <li className='dropdown-item'>Ornamental</li>
-                            </Link>
-                            <Link to="/fence-details/chain-link" onClick={closeMenu}>
-                                <li className='dropdown-item'>Chain Link</li>
-                            </Link>
-                        </ul>
-                    )}
-                    <Link to="/about" onClick={closeMenu}>About</Link>
-                    <Link to="/quote" onClick={closeMenu}>
-                        <Button style={{
-                            backgroundColor: 'white',
-                            color: '#0283AB',
-                            display: 'flex',
-                            alignItems: 'center',
-                            justifyContent: 'center',
-                            border: '2px solid #0283AB',
-                            fontWeight: 'bold',
-                            borderRadius: '5px',
-                            padding: '0 1rem',
-                            textTransform: 'none',
-                            transition: 'opacity 0.3s',
-                        }}
-                            onMouseEnter={e => e.target.style.opacity = 0.8}
-                            onMouseLeave={e => e.target.style.opacity = 1}
-                        >
-                            Instant Quote
-                        </Button>
-                    </Link>
-                </Menu>
-                {/* This ul will be hidden on smaller screens via CSS */}
-                <ul className='h-28 flex flex-row justify-between items-center gap-x-48 mr-4 desktop-menu'>
-                    <li className='nav-link text-xl'>
-                        <Link to='/'>Home</Link>
-                    </li>
-                    <li className='relative nav-link text-thin text-xl' onMouseEnter={toggleDropdown} onMouseLeave={toggleDropdown}>
-                        <Link to='/fence-types'>Fence Types</Link>
-                        {showDropdown && (
-                            <ul className="absolute bg-white mt-1 p-2 rounded-lg shadow-lg blue-border forefront">
-                                <Link to="/fence-details/cedar">
-                                    <li className='dropdown-item'>Cedar</li>
-                                </Link>
-                                <Link to="/fence-details/vinyl">
-                                    <li className='dropdown-item'>Vinyl</li>
-                                </Link>
-                                <Link to="/fence-details/ornamental">
-                                    <li className='dropdown-item'>Ornamental</li>
-                                </Link>
-                                <Link to="/fence-details/chain-link">
-                                    <li className='dropdown-item no-bottom'>Chain Link</li>
-                                </Link>
-                            </ul>
-                        )}
-                    </li>
-                    <li className='nav-link text-xl'>
-                        <Link to='/about'>About</Link>
-                    </li>
-                    <li style={{ height: '50%' }}>
-                        <Link to='/quote'>
-                            <Button style={{
-                                backgroundColor: 'white',
-                                color: '#0283AB',
-                                height: '100%',
-                                display: 'flex',
-                                alignItems: 'center',
-                                justifyContent: 'center',
-                                border: '2px solid #0283AB',
-                                fontWeight: 'bold',
-                                borderRadius: '5px',
-                                padding: '0 1rem',
-                                textTransform: 'none',
-                                transition: 'opacity 0.3s',
-                            }}
-                                onMouseEnter={e => e.target.style.opacity = 0.8}
-                                onMouseLeave={e => e.target.style.opacity = 1}
-                            >
-                                Instant Quote
-                            </Button>
-                        </Link>
-                    </li>
-                </ul>
-                {isXsScreen || isSmScreen ? <div>
+                        </li>
+                    </ul>
+                }
+                {isXsScreen || isSmScreen || isMdScreen || isLgScreen ? <div>
                     <Link to='/'>
                         <img src='../images/logos/nates-logo.jpg' className='h-28 ml-2 '></img>
                     </Link>
