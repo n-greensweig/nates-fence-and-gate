b03a1be71262c32e6f48e5275ba366479cf52085
Noah Greensweig <ngreensweig@gmail.com>
2024-02-20 10:06:15 -0600

    organize FenceDetails description on desktop

--- src/Views/FenceDetails/FenceDetails.jsx
+++ src/Views/FenceDetails/FenceDetails.jsx
@@ -39,7 +39,7 @@ function FenceDetails() {
         <div>
             {isXsScreen || isSmScreen ? <MobileFenceDetails /> :
                 <div>
-                    <h1 style={{ fontSize: '2rem' }}>{name}</h1>
+                    <h1 style={{ fontSize: '2rem', marginBottom: '3%' }}>{name}</h1>
                     {/* Conditionally render based on whether name === 'Cedar' */}
 
                     <div id="carousel-description"
@@ -48,17 +48,17 @@ function FenceDetails() {
                             flexDirection: 'row',
                             justifyContent: 'flex-start',
                             width: '100%',
+                            // border: '2px solid green',
                         }}>
-
-
-                        <ImageCarousel images={images} name={name} />
 
                         <div style={{
                             width: '100%',
+                            height: '90%',
                             display: 'flex',
                             flexDirection: 'column',
                             justifyContent: 'center',
                             alignItems: 'center',
+                            // border: '2px solid blue',
                         }}>
                             {/* Fence type description */}
                             <p
@@ -68,13 +68,17 @@ function FenceDetails() {
                                     justifyContent: 'center',
                                     fontSize: '1.2em',
                                     textAlign: 'left',
-                                    padding: isCedarPage ? '0 20% 3% 20%' : null,
+                                    padding: isCedarPage ? '0% 20% 3% 20%' : '0% 10% 3% 10%',
                                     margin: '0 auto',
+                                    // border: '2px solid red',
                                 }}
                             >{fenceType ? fenceType.description : null}</p>
                             <ButtonComponent type={'Quote'} />
                         </div>
 
+                        <div style={{ width: 'fit-content' }}>
+                            <ImageCarousel images={images} name={name} />
+                        </div>
                     </div>
                 </div>
             }
