0d8b4fda9a73370270e42c1887d56992c1133158
Noah Greensweig <ngreensweig@gmail.com>
2025-02-04 22:26:41 -0600

    Fixed canonical error problems to help with SEO.

--- src/Views/CedarDetails/CedarDetails.jsx
+++ src/Views/CedarDetails/CedarDetails.jsx
@@ -1,31 +1,53 @@
+import React from "react";
+import { Helmet } from "react-helmet";
 import { fenceTypes } from "../../Constants/Constants";
 import TypeCard from "../../Components/TypeCard/TypeCard";
-import { Grid } from "@mui/material";
-
-// Responsive imports
-import { useMediaQuery, useTheme } from "@mui/material";
+import { Grid, useMediaQuery, useTheme } from "@mui/material";
+import { useLocation } from "react-router-dom";
 
 function CedarDetails() {
+  const location = useLocation();
+  const cedarTypes = fenceTypes[0].cedarTypes;
 
-    const cedarTypes = fenceTypes[0].cedarTypes;
+  // Check the screen size for responsive design
+  const theme = useTheme();
+  const isXsScreen = useMediaQuery(theme.breakpoints.down("xs"));
+  const isSmScreen = useMediaQuery(theme.breakpoints.down("sm"));
 
-    // Check the screen size for responsive design
-    const theme = useTheme();
-    const isXsScreen = useMediaQuery(theme.breakpoints.down("xs"));
-    const isSmScreen = useMediaQuery(theme.breakpoints.down("sm"));
+  // Use the current pathname to build the canonical URL
+  const canonicalUrl =
+    process.env.NODE_ENV === "production"
+      ? `https://www.natesfenceandgate.com/fence-details/cedar`
+      : window.location.href;
 
-    return (
-        <div className="bg-slate-100 pb-16 pt-1" style={{ display: 'flex', justifyContent: 'center', margin: '3% auto 0 auto', }}>
-            <Grid container spacing={2} style={{ maxWidth: '1300px', width: isXsScreen || isSmScreen ? '85%' : null  }}>
-                {cedarTypes.map((cedarType, index) => (
-                    <Grid item xs={12} sm={6} md={4} lg={4} key={index}>
-                        <TypeCard fenceType={cedarType} />
-                    </Grid>
-                ))}
+  return (
+    <>
+      {/* Add canonical link for SEO */}
+      <Helmet>
+        <link rel="canonical" href={canonicalUrl} />
+      </Helmet>
+      <div
+        className="bg-slate-100 pb-16 pt-1"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          margin: "3% auto 0 auto",
+        }}
+      >
+        <Grid
+          container
+          spacing={2}
+          style={{ maxWidth: "1300px", width: isXsScreen || isSmScreen ? "85%" : null }}
+        >
+          {cedarTypes.map((cedarType, index) => (
+            <Grid item xs={12} sm={6} md={4} lg={4} key={index}>
+              <TypeCard fenceType={cedarType} />
             </Grid>
-        </div>
-    )
-
+          ))}
+        </Grid>
+      </div>
+    </>
+  );
 }
 
 export default CedarDetails;
--- src/Views/FenceDetails/FenceDetails.jsx
+++ src/Views/FenceDetails/FenceDetails.jsx
@@ -1,30 +1,27 @@
 // Views/FenceDetails/FenceDetails.jsx
 import { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate, useLocation } from "react-router-dom";
 import { fenceTypes } from "../../Constants/Constants";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import ButtonComponent from "../../Components/Button/Button";
-
 import ImageCarousel from "../../Components/ImageCarousel/ImageCarousel";
 import MobileFenceDetails from "../MobileFenceDetails/MobileFenceDetails";
-import { useNavigate, useLocation } from "react-router-dom";
-
-// Responsive imports
 import { useMediaQuery } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
+import { Helmet } from "react-helmet";
 
 function FenceDetails() {
-
     const { type } = useParams();
     const navigate = useNavigate();
     const location = useLocation();
     console.log(location.pathname);
 
-    const fenceType = fenceTypes.find(f => f.route === type) ? fenceTypes.find(f => f.route === type) :
-        fenceTypes[0].cedarTypes.find(f => f.route === type); // Find the fence type that matches the route
+    const fenceType = fenceTypes.find(f => f.route === type)
+        ? fenceTypes.find(f => f.route === type)
+        : fenceTypes[0].cedarTypes.find(f => f.route === type); // Find the fence type that matches the route
+
     const name = fenceType.name; // Get the name of the fence type
     const images = fenceType.images; // Get the images of the fence type
-
     const isCedarPage = location.pathname === '/fence-details/cedar'; // Check if the current page is the Cedar Fence Details page
 
     // Check the screen size for responsive design
@@ -36,31 +33,45 @@ function FenceDetails() {
         if (fenceType) {
             document.title = `${fenceType.name} Fence Details`;
         }
+        console.log(location.pathname);
     }, [fenceType]);
+
+    // Create a dynamic canonical URL based on the current path
+    const canonicalUrl =
+        process.env.NODE_ENV === "production"
+            ? `https://www.natesfenceandgate.com${location.pathname}`
+            : window.location.href;
 
     return (
         <div>
-            {isXsScreen || isSmScreen ? <MobileFenceDetails /> :
+            <Helmet>
+                <link rel="canonical" href={canonicalUrl} />
+            </Helmet>
+            {isXsScreen || isSmScreen ? (
+                <MobileFenceDetails />
+            ) : (
                 <div>
                     <h1 style={{ fontSize: '2rem', marginBottom: '3%' }}>{name}</h1>
                     {/* Conditionally render based on whether name === 'Cedar' */}
-
-                    <div id="carousel-description"
+                    <div
+                        id="carousel-description"
                         style={{
                             display: 'flex',
                             flexDirection: 'row',
                             justifyContent: 'flex-start',
                             width: '100%',
-                        }}>
-
-                        <div style={{
-                            width: '100%',
-                            height: '90%',
-                            display: 'flex',
-                            flexDirection: 'column',
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                        }}>
+                        }}
+                    >
+                        <div
+                            style={{
+                                width: '100%',
+                                height: '90%',
+                                display: 'flex',
+                                flexDirection: 'column',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                            }}
+                        >
                             {/* Fence type description */}
                             <p
                                 style={{
@@ -72,16 +83,17 @@ function FenceDetails() {
                                     padding: isCedarPage ? '0% 20% 3% 20%' : '0% 10% 3% 10%',
                                     margin: '0 auto',
                                 }}
-                            >{fenceType ? fenceType.description : null}</p>
+                            >
+                                {fenceType ? fenceType.description : null}
+                            </p>
                             <ButtonComponent type={'Quote'} />
                         </div>
-
                         <div style={{ width: 'fit-content' }}>
                             <ImageCarousel images={images} name={name} />
                         </div>
                     </div>
                 </div>
-            }
+            )}
         </div>
     );
 }
